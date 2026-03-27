dda84503bcc0d7e530f7cf77f40077cb81b5f0e5
Piyush Jadhav <piyushsj02@gmail.com>
2026-03-27 23:13:39 +0530

    register

---
+++ backend/src/main/java/com/ecommerce/backend/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.ecommerce.backend.controller;
+
+import com.ecommerce.backend.entity.User;
+import com.ecommerce.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+@CrossOrigin
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.register(user);
+    }
+}
